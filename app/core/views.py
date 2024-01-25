2421d4c5ff6531bd723cf034aeecfff2d413e546
Sesugh Agbadu <sesughagbadu@nvit.tech>
2024-01-25 11:58:52 +0100

    add world bank

--- app/core/views.py
+++ app/core/views.py
@@ -1,15 +1,47 @@
 from flask import render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
+from sqlalchemy import func
 
 from models.student import Student
+from utils.common import course_code
 from . import core_bp
 
 
 @core_bp.route('/', methods=['GET'])
 @login_required
 def dashboard():
+    #global grouped, female_count, male_count
     students = len(Student.query.all())
+
+    if current_user.is_authenticated and current_user.role == 'user' and current_user.username == 'worldbank':
+        pass
+        #print(grouped)
+
+    female_count = Student.query.filter_by(gender='Female').count()
+    male_count = Student.query.filter_by(gender='Male').count()
+    grouped = Student.query.with_entities(Student.course_of_study, func.count(Student.course_of_study)).group_by(
+        Student.course_of_study).all()
+
+    labels = list()
+    values = list()
+    courses = list()
+
+    for course in grouped:
+        code = course_code(course[0])
+        labels.append(code)
+        values.append(course[1])
+        print(f'course val: {course[1]}')
+        courses.append(course[0])
+
+    print(values)
     data = {
-        'captured': students
+        'captured': students,
+        'stats': {
+            'labels': labels,
+            'data': values,
+            'courses': courses
+        },
+        'female_count': female_count,
+        'male_count': male_count
     }
     return render_template('dashboard.html', records=data)
--- app/student/views.py
+++ app/student/views.py
@@ -1,17 +1,45 @@
 from flask import render_template
+from flask_login import login_required
+
+from models.student import Student
 from . import student_bp
 
 
-@student_bp.route('/list')
+@student_bp.route("/list")
+@login_required
 def list():
-    return render_template('student/list.html')
+    return render_template("student/list.html")
 
 
-@student_bp.route('/student/<int:student_id>')
+@student_bp.route("/student/<int:student_id>")
+@login_required
 def post(student_id):
-    return render_template('student/details.html', student_id=student_id)
+    return render_template("student/details.html", student_id=student_id)
 
 
-@student_bp.route('/create')
+@student_bp.route("/create")
+@login_required
 def register():
-    return render_template('student/create.html')
+    return render_template("student/create.html")
+
+
+@student_bp.route("/ablum")
+@login_required
+def album_list():
+    students = Student.query.with_entities(
+        Student.id,
+        Student.means_of_id_no,
+        Student.first_name,
+        Student.last_name,
+        Student.middle_name,
+        Student.gender,
+        Student.course_of_study,
+        Student.student_no,
+        Student.phone_number,
+        Student.email,
+        Student.lga_of_origin,
+        Student.state_of_origin,
+    ).all()
+    return render_template(
+        "student/cards.html", students=students, title="Student Album"
+    )
