80c006edbce81ace96c2b8ef7ea11aaedb1bc388
Sesugh Agbadu <sesughagbadu@nvit.tech>
2024-02-01 11:47:36 +0100

    update nin retrieval logic

--- app/core/views.py
+++ app/core/views.py
@@ -347,5 +347,5 @@ def nin_update():
 
         return redirect(url_for('core.nin_update'))
 
-    students = Student.query.with_entities(Student.id, Student.student_no, Student.first_name, Student.middle_name, Student.last_name, Student.lga_of_origin, Student.headshot, Student.means_of_id_no, Student.gender, Student.record_sealed).filter(Student.record_sealed != False).order_by(Student.id).all()
+    students = Student.query.with_entities(Student.id, Student.student_no, Student.first_name, Student.middle_name, Student.last_name, Student.lga_of_origin, Student.headshot, Student.means_of_id_no, Student.gender, Student.record_sealed).filter(Student.record_sealed=='null').order_by(Student.id).all()
     return render_template('student/ninupdate.html', students=students, title='Student NIN Update')
