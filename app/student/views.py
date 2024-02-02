3c645f132d5fb1c76738862c132bf8f426774a9e
Sesugh Agbadu <sesughagbadu@nvit.tech>
2024-02-02 16:22:27 +0100

    nin update query optimization

--- app/student/views.py
+++ app/student/views.py
@@ -27,22 +27,7 @@ def register():
 @student_bp.route("/album")
 @login_required
 def album_list():
-    students = Student.query.filter((Student.employment_status != 'Employed')).all()  #db.session.execute(db.select(Student).order_by(Student.id)).scalars().all()
-    # students = Student.query.with_entities(
-    #     Student.id,
-    #     Student.means_of_id_no,
-    #     Student.first_name,
-    #     Student.last_name,
-    #     Student.middle_name,
-    #     Student.gender,
-    #     Student.course_of_study,
-    #     Student.student_no,
-    #     Student.phone_number,
-    #     Student.email,
-    #     Student.lga_of_origin,
-    #     Student.state_of_origin,
-    #     Student.headshot
-    # ).order_by(Student.id).all()
+    students = Student.query.filter((Student.employment_status != 'Employed')).order_by(Student.id).all()  #db.session.execute(db.select(Student).order_by(Student.id)).scalars().all()
     return render_template(
         "student/card.html", students=students, title="Student Album"
     )
